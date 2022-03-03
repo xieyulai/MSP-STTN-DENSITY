6f347de49b836bcff03d57501c4aa8cf5f494a47
x@g5 <x@g5>
2022-03-03 09:22:06 +0800

    1712

--- ALL.sh
+++ ALL.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-RECORD=5720
+RECORD=5721
 
 ###TRAIN
 python3 pre_main_short.py --mode train --record $RECORD 
 ###TEST
 python3 pre_main_short.py --mode val --record $RECORD 
-
 
 tail record/$RECORD/log.txt -n 1
