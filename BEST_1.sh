4694f4608304104e29eece0477a5902de6b331d6
x@g5 <x@g5>
2022-03-03 09:19:58 +0800

    3711

---
+++ BEST_1.sh
@@ -0,0 +1,7 @@
+#0!/bin/bash
+#DIM512
+RECORD=1712
+#DIM256
+#RECORD=5712
+python3 pre_main_short.py --best 1 --mode val --record $RECORD 
+tail record/$RECORD/log.txt -n 1
--- BEST_2.sh
+++ BEST_2.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-RECORD=5712
+#DIM256
+#RECORD=5720
+#DI512M
+RECORD=3711
 python3 pre_main_short.py --best 1 --mode val --record $RECORD 
 tail record/$RECORD/log.txt -n 1
